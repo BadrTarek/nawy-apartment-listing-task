00f10d34d328d9331b9c32245505703d886ac12d
BadrTarek <badr.tarek20@gmail.com>
2025-05-14 21:53:06 +0300

    Refactor CreateApartmentService to map features to apartmentFeatureMappings before bulk creation

--- apps/backend/src/application/services/create-apartment.service.ts
+++ apps/backend/src/application/services/create-apartment.service.ts
@@ -1,6 +1,7 @@
 import { Apartment as ApartmentEntity } from "../../domain/entities/apartment.entity";
 import { IUnitOfWork } from "../../domain/interfaces/unit-of-work.interface";
 import { CreateApartmentDto } from "../dtos/create-apartment.dto";
+import { ApartmentFeatureMapping } from '../../domain/entities/apartment-feature-mapping.entity';
 
 
 
@@ -8,10 +9,18 @@ import { CreateApartmentDto } from "../dtos/create-apartment.dto";
 export class CreateApartmentService {
     constructor(
         private readonly unitOfWork: IUnitOfWork
-    ) {}
+    ) { }
 
     async execute(createApartmentDto: CreateApartmentDto): Promise<ApartmentEntity> {
         const apartment = await this.unitOfWork.apartmentRepository.create(createApartmentDto);
+        const apartmentFeatureMappings = createApartmentDto.features.map(feature => {
+            return {
+                apartmentId: apartment.id,
+                featureId: feature.featureId,
+                featureValue: feature.featureValue
+            };
+        });
+        await this.unitOfWork.apartmentFeatureMappingRepository.bulkCreate(apartmentFeatureMappings as ApartmentFeatureMapping[]);
         return apartment;
     }
 }
