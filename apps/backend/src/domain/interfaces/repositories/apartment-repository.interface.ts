ccf6c649a018ae625cd6bb11ac2cd90b93ca21f1
BadrTarek <badr.tarek20@gmail.com>
2025-05-14 11:25:26 +0300

    Update IApartmentRepository to use number type for getById parameter

--- apps/backend/src/domain/interfaces/repositories/apartment-repository.interface.ts
+++ apps/backend/src/domain/interfaces/repositories/apartment-repository.interface.ts
@@ -3,6 +3,6 @@ import { Apartment } from "../../entities/apartment.entity";
 
 export interface IApartmentRepository {
     list(): Promise<Apartment[]>;
-    getById(id: string): Promise<Apartment | null>;
+    getById(id: number): Promise<Apartment | null>;
     create(apartment: Apartment): Promise<Apartment>;
 }
