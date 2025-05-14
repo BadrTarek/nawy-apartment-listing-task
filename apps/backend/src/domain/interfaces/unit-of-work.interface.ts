00a403c2082042aeb8064a4cfa2c2873f83defe2
BadrTarek <badr.tarek20@gmail.com>
2025-05-14 13:14:35 +0300

    Add beginTransaction method to IUnitOfWork interface for transaction management

--- apps/backend/src/domain/interfaces/unit-of-work.interface.ts
+++ apps/backend/src/domain/interfaces/unit-of-work.interface.ts
@@ -7,6 +7,7 @@ export interface IUnitOfWork {
     apartmentRepository: IApartmentRepository;
     
     // Methods
+    beginTransaction(): Promise<void> 
     commit(): Promise<void>;
     rollback(): Promise<void>;
 }
